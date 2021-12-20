4f2690c433f2f0cfb15b07edf5f7fa6ba061aba1
Brian <brian@email.org>
2021-12-20 16:57:05 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Navbar
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+//Navbar Toggle
+function navToggle() {
+  btn.classList.toggle('open')
+  nav.classList.toggle('hidden')
+  document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle)
